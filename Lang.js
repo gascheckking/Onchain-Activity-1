bf2dcf79beb417100a647ad36f9b7091a2a3c4a8
gascheckking <warpaiactivity1@gmail.com>
2025-05-21 16:57:22 +0200

    Create Lang.js

---
+++ Lang.js
@@ -0,0 +1,17 @@
+// lang.js
+const lang = {
+  en: {
+    overview: "Overview",
+    wallet: "Wallet",
+    leaderboard: "Leaderboard",
+    rewards: "Rewards",
+    settings: "Settings"
+  },
+  sv: {
+    overview: "Översikt",
+    wallet: "Plånbok",
+    leaderboard: "Topplista",
+    rewards: "Belöningar",
+    settings: "Inställningar"
+  }
+};
